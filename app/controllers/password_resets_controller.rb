490081a2b5bb071672d3b1d2676151026cb25bce
eswari-tamu <eswari.choudary@tamu.edu>
2023-09-27 17:33:45 -0500

    Add reset_password feature

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,36 @@
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user = User.find_by_email_address(params[:email_address])
+      PasswordMailer.with(user: @user).reset.deliver_later
+    end
+    redirect_to '/login', notice: 'If an account with that email was    found, we have sent a link to reset password'
+  
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to '/login', alert: 'Your token has expired. Please try again'
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    if @user.update(password_params)
+      redirect_to '/login', notice: 'Your password was reset succesfully. Please sign in.'
+    else
+      render 'edit'
+    end
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to '/login', alert: 'Your token has expired. Please try again'
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/password/reset', to: 'password_resets#new'
+  post '/password/reset', to: 'password_resets#create'
+  get '/password/reset/edit', to: 'password_resets#edit'
+  patch '/password/reset/edit', to: 'password_resets#update'
 end
