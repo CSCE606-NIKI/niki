1a2312cf7eca4e591ad4060f894d51675d5b7f62
Askani-Priyanka <142848889+Askani-Priyanka@users.noreply.github.com>
2023-09-26 23:56:53 -0500

    Add cucumber tests for signup

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,6 +7,11 @@ Given('I am on the login page') do
     visit login_path 
 end
   
+
+Given('I am new user and I am on the user new page') do
+    visit new_user_path 
+end
+
 When('I fill in {string} with {string}') do |string, string2|
     fill_in string, with: string2
 end
