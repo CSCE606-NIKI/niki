5fc54d294b62444b2dd7fde8e0166acca7217d9e
iskonopleva <isko@tamu.edu>
2023-09-29 12:47:08 -0500

    heroku push

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    provider :google_oauth2, '590415397459-aevkphkq0dove73f9nfo9kr8euf2a8bs.apps.googleusercontent.com', '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',
     scope: "userinfo.email, userinfo.profile",
     prompt: "select_account"
   end
