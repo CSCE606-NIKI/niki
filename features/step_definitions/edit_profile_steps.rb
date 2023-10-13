c85c90045789001e13cd42762ee424ceb18aecaf
Askani-Priyanka <142848889+Askani-Priyanka@users.noreply.github.com>
2023-10-12 21:00:21 -0500

    Fix cucumber test

--- features/step_definitions/edit_profile_steps.rb
+++ features/step_definitions/edit_profile_steps.rb
@@ -1,14 +1,5 @@
 # features/step_definitions/edit_profile_steps.rb
 
-Given('I am a registered user and I am on the login page') do
-    @mock_user = create(:user,username: 'mockuser', email: 'mock@gmail.com' , password: 'password') 
-    visit login_path 
-end
-
-Given('I am on the login page') do
-    visit login_path 
-end
-
 When("I visit the edit profile page") do
   visit profile_edit_path(@current_user)
 end
@@ -17,7 +8,6 @@ When("I attach the file {string} to {string}") do |file, field|
   puts File.join(Rails.root, file)
   attach_file(field, File.join(Rails.root, file))
 end
-
 
 When("I update my profile with the following information:") do |table|
   data = table.hashes.first
