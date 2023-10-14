37fc27322a6b039a032934424b2b2821a88cd505
eswari-tamu <eswari.choudary@tamu.edu>
2023-10-14 17:19:32 -0500

    Add app gmail credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'localhost:3000',
-    user_name: Rails.application.credentials(gmail_smtp, email),
-    password: Rails.application.credentials(gmail_smtp, password),
+    user_name: Rails.application.credentials.dig(:gmail_smtp, :email),
+    password: Rails.application.credentials.dig(:gmail_smtp, :password),
     authentication: 'plain',
     enable_starttls_auto: true
   }
