77623941570f9bc8e0bcce8fb092c95b7b3d4e99
eswari-tamu <eswari.choudary@tamu.edu>
2023-11-10 11:09:26 -0600

    Bug Resolve ForgotPassword

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "csce606-niki.herokuapp.com", protocol: "https"}
+  config.action_mailer.default_url_options = { host: "csce606-niki-a5715460073c.herokuapp.com", protocol: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_10_080232) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_080232) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.integer "user_id"
     t.index ["user_id"], name: "index_credit_types_on_user_id"
   end
 
@@ -58,12 +55,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_080232) do
     t.date "date"
     t.integer "amount"
     t.string "credit_type"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "creditID"
-    t.bigint "credit_type_id", default: 1, null: false
+    t.integer "credit_type_id", default: 1, null: false
     t.index ["credit_type_id"], name: "index_credits_on_credit_type_id"
     t.index ["user_id"], name: "index_credits_on_user_id"
   end
