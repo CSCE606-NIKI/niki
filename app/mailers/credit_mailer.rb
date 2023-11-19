b3a5e88877373914c42b8bf570b668348bc7089f
Askani-Priyanka <askani2407@tamu.edu>
2023-11-18 21:23:59 -0600

    Credit report

--- app/mailers/credit_mailer.rb
+++ app/mailers/credit_mailer.rb
@@ -2,9 +2,10 @@
 class CreditMailer < ApplicationMailer
   def send_pending_credits_email(user, credit_progress)
     @user = user
-    @credits_progress = credit_progress
+    @credit_progress = credit_progress
     @token = @user.signed_id(purpose: 'pending_credits', expires_in: 30.minutes)
     mail(to: @user.email, subject: 'Pending Credits to complete in the Renewal period.') do |format|
       format.html { render 'send_pending_credits_email' }
+    end
   end
 end
