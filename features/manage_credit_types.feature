417a86a7f12c1419f058835ae5e51b0f45afc693
eswari-tamu <eswari.choudary@tamu.edu>
2023-11-20 15:16:49 -0600

    Edit and delete creditType

--- features/manage_credit_types.feature
+++ features/manage_credit_types.feature
@@ -23,23 +23,39 @@ Feature: Manage Credit Types
 
   Scenario: Edit a existed credit type
     Given I have added a credit type
-    Then I should see the "Show this Credit Type" link for the new credit type
+    When I visit the Add New credit page
+    And I fill in the following:
+    | Field        | Value       |
+    | Credit type  | Type        |
+    | Date         | 2023-10-11  |
+    | Amount       | 3           |
+    | Description  | Test credit |
+    And I click the "Create Credit" button
+    Then I should be redirected to the dashboard
+    And I should see the "Show this Credit Type" link for the new credit type
     When I click link "Show this Credit Type"
     Then I should see Edit button
     When I click on Edit button
     Then I should be directed to edit_credit_type_path
     When I fill in "Credit limit" with "10"
-    And click Update Credit Type
-    Then I should be directed to Dashboard
+    And click Update Credit type
+    Then I should be redirected to Dashboard
+    And should see a message
 
-# Scenario: Delete credit type
-# When I visit the new credit type page
-# And I fill in "Name" with "Type1"
-# And I fill in "Credit limit" with "1000"
-# And I check "Carry forward"
-# And I press "Create Credit type"
-# When I click link "Show this Credit Type"
-# Then I should see Delete button
-# When I click on Delete button
-# Then I should be directed to Dashboard
-# And I shouldnot see Type1 Credit type
+  Scenario: Delete credit type
+    Given I have added a credit type
+    When I visit the Add New credit page
+    And I fill in the following:
+    | Field        | Value       |
+    | Credit type  | Type        |
+    | Date         | 2023-10-11  |
+    | Amount       | 3           |
+    | Description  | Test credit |
+    And I click the "Create Credit" button
+    Then I should be redirected to the dashboard
+    And I should see the "Show this Credit Type" link for the new credit type
+    When I click link "Show this Credit Type"
+    Then I should see Delete button
+    When I click on Delete button
+    Then I should be redirected to Dashboard
+    And should see a deletion message
--- features/step_definitions/credit_types_steps.rb
+++ features/step_definitions/credit_types_steps.rb
@@ -21,34 +21,17 @@ When("I press {string}") do |button|
 end
 
 Given("I have added a credit type") do
-#    @credit_type = @user.credit_types.create(name: "Type", credit_limit: 100, carry_forward: true)
-#    raise "Credit type not created successfully" unless @user.credit_types.count > 0
-   @new_credit_type_name = "New Type"
-   @user.credit_types.create(name: @new_credit_type_name, credit_limit: 100, carry_forward: true)
+   @credit_type = @user.credit_types.create(name: "Type", credit_limit: 100, carry_forward: true)
+   raise "Credit type not created successfully" unless @user.credit_types.count > 0
 end
 
-# When(/^I click link "([^"]*)"$/) do |link_text|
-#     puts page.html
-#     click_link(link_text) 
-# end
 When("I should see the {string} link for the new credit type") do|string|
-#    expect(page).to have_link(string, href: credit_type_path(CreditType.find_by(name: @new_credit_type_name)))
-   puts page.body
-   puts @credit_totals.inspect
-   expect(page).to have_link("Show this Credit Type", href: "/credit_types/1")
+   expect(page).to have_link("Show this Credit Type", href: "/credit_types/Type")
 end
 
 When(/^I click link "Show this Credit Type"$/) do
     puts @credit_totals.inspect
     click_link("Show this Credit Type")
-    # @credit_type = CreditType.find_by(name: 'Type')
-    # puts "Credit Type ID: #{@credit_type.id}"
-
-    # # Print the HTML of the page for debugging purposes
-    # puts page.html
-
-    # # Click the link with a specific selector and wait for up to 10 seconds
-    # find('table.styled-table td a', text: 'Show this Credit Type', wait: 10).click
 end
 
 When("I click {string}") do |button|
@@ -59,38 +42,36 @@ Then('I should see Edit button') do
     expect(page).to have_button('Edit')
 end
 
-When('I click on {string} button') do |button|
-    click_button button
+When("I click on Edit button") do
+    click_button "Edit"
 end
 
-Then('I should be directed to {string}') do |path|
-    visit path
+Then("I should be directed to edit_credit_type_path") do
+    @credit_type = CreditType.find_by(name: 'Type')
+    visit edit_credit_type_path(@credit_type)
 end
 
-When('click Update Credit type') do
-    click_button 'Update Credit type'
+And("click Update Credit type") do
+    puts "Current Path: #{current_path}"
+    click_button "Update Credit type"
 end
 
-Then('I should be directed to Dashboard') do
-    visit dashboard_path
+Then("I should be redirected to Dashboard") do
+    expect(current_path).to eq(dashboard_path)
 end
 
+Then('should see a message') do
+    expect(page).to have_content('Credit type updated successfully.')
+end
+# delete .......
 Then('I should see Delete button') do
     expect(page).to have_button('Delete')
 end
 
-Then('I shouldnot see Type1 Credit type') do
-    pending # Write code here that turns the phrase above into concrete actions
+When('I click on Delete button') do
+    click_button "Delete"
 end
 
-When('I click on Edit button') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('I should be directed to edit_credit_type_path') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-When('click Update Credit Type') do
-    pending # Write code here that turns the phrase above into concrete actions
+Then('should see a deletion message') do
+    expect(page).to have_content('Credit type was successfully deleted.')
 end
