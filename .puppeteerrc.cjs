3f3bab9d862251b1aa03a6f65cccdcff09a6acb7
iskonopleva <isko@tamu.edu>
2023-11-21 15:37:34 -0600

    Fixed print to PDF

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -7,10 +7,3 @@ module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
-
-const browser = await puppeteer.launch({
-    'args' : [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-  });
