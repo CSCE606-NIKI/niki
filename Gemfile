419888cff877d328c723e656ff2d1e7d4b72b38e
iskonopleva <isko@tamu.edu>
2023-11-21 14:19:13 -0600

    Fixed print to PDF

--- Gemfile
+++ Gemfile
@@ -46,12 +46,6 @@ gem 'sidekiq-scheduler'
 gem 'sidekiq-cron'
 gem 'activesupport',  '7.0.7.2'
 gem 'bootstrap', '~> 5.1.3'
-gem 'wicked_pdf'
-group :development do
-  gem 'wkhtmltopdf-binary'
-end
-group :production do
-  gem 'wkhtmltopdf-heroku'
-end
+gem 'grover'
 
   
--- spec/requests/prints_spec.rb
+++ spec/requests/prints_spec.rb
@@ -1,18 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Prints", type: :request do
-#   describe "GET /index" do
-#     it "returns http success" do
-#       get "/prints/index"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
+RSpec.describe "Prints", type: :request do
+  describe "GET /show" do
+    it "returns http success" do
+      get "/prints/show"
+      expect(response).to have_http_status(:redirect)
+    end
+  end
 
-#   describe "GET /show" do
-#     it "returns http success" do
-#       get "/prints/show"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-# end
+end
