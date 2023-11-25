d80fd557f88d840e1420e9a508905698bc21fde6
iskonopleva <isko@tamu.edu>
2023-11-24 21:53:09 -0600

    Fixed admin console vulnerability

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,16 +1,10 @@
 class AdminController < ApplicationController
     layout false
-    def index
-        if logged_in?
-            @user = current_user
-            if (@user.admin != true)
-                render :logout
-            end
+    before_action :require_admin_login
 
-            @users = User.all
-        else
-            redirect_to admin_login_path
-        end
+    def index
+        @user = current_user
+        @users = User.all
     end
 
     def user_overview
@@ -22,18 +16,14 @@ class AdminController < ApplicationController
     def change_user_role
         user_id = params[:u]
         @user = User.find_by(id: user_id)
-        if @user != nil
-            if @user.admin == true
-                @user.admin = false
-                @user.save!
-            else
-                @user.admin = true
-                @user.save!
-            end
-            redirect_to user_overview_path(:u => @user.id)
+        if @user.admin == true
+            @user.admin = false
+            @user.save!
         else
-            flash[:error] = "User doesn't exist"
+            @user.admin = true
+            @user.save!
         end
+        redirect_to user_overview_path(:u => @user.id), notice: "Role changed successfully"
     end
 
     def delete_user
@@ -42,5 +32,18 @@ class AdminController < ApplicationController
         @user_credits = Credit.where(user: @user)
         User.destroy(user_id)
         redirect_to admin_path, notice: "User deleted successfully"
+    end
+
+    private
+    def require_admin_login
+        if logged_in?
+            if current_user.admin != true
+                flash[:error] = "You do not have access to this resource. Log out and try different credentials."
+                render :logout
+            end
+        else
+        flash[:error] = "You must be logged in to access the dashboard."
+        redirect_to admin_login_path
+        end
     end
 end
